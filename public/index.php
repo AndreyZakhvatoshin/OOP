8b60d671a85be31279628d87cf323a4e9bab0e7c
Andrey Zakhvatoshin <mastetandron@gmail.com>
2020-05-08 22:17:54 +0500

    Add .lock

--- public/index.php
+++ public/index.php
@@ -1,9 +1,22 @@
 <?php
 
 declare(strict_types=1);
+use DI\ContainerBuilder;
+use Relay\Relay;
+use Zend\Diactoros\ServerRequestFactory;
+use function DI\create;
+use App\Models\Name;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-$user = new \App\Models\Name('Andrey', 'Zakhvatoshin');
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    Name::class => create(Name::class)
+]);
 
-$user->sayHello();
+$container = $containerBuilder->build();
+$name = $container->get(Name::class);
+
+$name->sayHello();
