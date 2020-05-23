82ec2ae24fd63c8532021e062763bafde54a86e9
Andrey Zakhvatoshin <mastetandron@gmail.com>
2020-05-23 15:19:16 +0500

    Add footer

--- src/Views/templates/template.php
+++ src/Views/templates/template.php
@@ -14,49 +14,65 @@
 </head>
 
 <body>
-    <header class="header-nav">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-3">
+    <div class="page">
+        <header class="header-nav">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
 
-                </div>
-                <div class="col-md-7">
-                    <ul class="nav justify-content-center">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">Главная страница</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/posts">Статьи</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/about">Обо мне</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-2">
-                    <ul class="nav justify-content-center">
-                        <li class="nav-item">
-                            <?php if(empty($_SESSION)): ?>
-                                <a class="nav-link" href="/sign-in">Вход</a>
-                            <?php else: ?>
-                                <a class="nav-link" href="/exit">Выход</a>
-                            <?php endif; ?>
-                        </li>
-                        <li class="nav-item">
-                            <?php if(empty($_SESSION)): ?>
-                                <a class="nav-link" href="/sign-up">Регистрация</a>
-                            <?php endif; ?>
-                        </li>
-                    </ul>
+                    </div>
+                    <div class="col-md-7">
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Главная страница</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/posts">Статьи</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/about">Обо мне</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2">
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item">
+                                <?php if (empty($_SESSION)) : ?>
+                                    <a class="nav-link" href="/sign-in">Вход</a>
+                                <?php else : ?>
+                                    <a class="nav-link" href="/exit">Выход</a>
+                                <?php endif; ?>
+                            </li>
+                            <li class="nav-item">
+                                <?php if (empty($_SESSION)) : ?>
+                                    <a class="nav-link" href="/sign-up">Регистрация</a>
+                                <?php endif; ?>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
+        </header>
+        <div class="content">
+            <?= $this->section('content') ?>
         </div>
-
-
-
-    </header>
-    <?= $this->section('content') ?>
-
+        
+        <footer>
+            <div class="container-fluid">
+                <div class="row footer-content">
+                    <div class="col-md-3">
+                        <p class="info">Привет</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p class="copyright">2020</p>
+                    </div>
+                    <div class="col-md-3">
+                        <p class="email-adress">mastetandron@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
