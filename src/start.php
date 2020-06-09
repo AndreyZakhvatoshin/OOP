8ae89f03b5fe51c2f444075e44a488cd6c927422
Andrey Zakhvatoshin <mastetandron@gmail.com>
2020-06-09 10:14:02 +0500

    delete di to database

--- src/start.php
+++ src/start.php
@@ -1,11 +1,9 @@
 <?php
 
 use PDO;
-use DI\Container;
 use DI\ContainerBuilder;
 use League\Plates\Engine;
 use Aura\SqlQuery\QueryFactory;
-use App\Models\Database;
 
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions([
@@ -17,9 +15,6 @@ $containerBuilder->addDefinitions([
     },
     PDO::class => function() {
         return new PDO('mysql:dbname=blog;host=localhost;charset=utf8mb4', 'admin', 'password');
-    },
-    Database::class => function() {
-        return new Database(new QueryFactory('mysql'), new PDO('mysql:dbname=blog;host=localhost;charset=utf8mb4', 'admin', 'password'));
     }
 ]);
 
